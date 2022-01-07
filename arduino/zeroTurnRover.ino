7d16107ec4880c980ba0494c892c6ab020cc4ee7
skywo1f <ivan_viti@hotmail.com>
2022-01-07 07:22:58 -0500

    direct update
    fixed the 3 forms of scuffing detection

--- arduino/zeroTurnRover.ino
+++ arduino/zeroTurnRover.ino
@@ -30,13 +30,22 @@ float rpm2 = 0; // Value to be sent to Vesc 2
 float oldrpm1 = 0;// Hold previous value of rpm1 for smoothing
 float oldrpm2 = 0; //"  " rpm2
 float smooth = 0;
+//buzz related constants
 int n = 0;
+int buzzOn = 0;
+float angleLimit = PI/10;
+float thrustLimit = 5000;
 float accz;
 float accx;
+float accy;
+
 const int streamPeriod = 40;
 unsigned long lastStreamTime = 0;
 bool updateSensorData = true;
-
+double angle = 0;
+double sensorAngle = 0;//assumung 0 degree tilt on bno055 imu
+int maxTorque = 10000;       //should be between 0 and 28k
+float sideAccK = 3.0;       //between 0 and 9.8
 // TB6612FNG Standby Pin
 #define stby 6
  
@@ -47,8 +56,8 @@ int MotorSpeedB = 0;
 // Motor Direction Values - -1 = backward, 1 = forward
 int MotorDirA = 1;
 int MotorDirB = 1;
- 
-
+double runningDeltaThrust = 0; 
+double runningWeight = 0.5;
 //convert transceiver value to wheel value
 // Read the number of a given channel and convert to the range provided.
 // If the channel is off, return the default value
@@ -63,6 +72,60 @@ bool readSwitch(byte channelInput, bool defaultValue) {
   int intDefaultValue = (defaultValue) ? 100 : 0;
   int ch = readChannel(channelInput, 0, 100, intDefaultValue);
   return (ch > 50);
+}
+ double getAngle(double accz,double accy){
+  return atan(accy/accz) + sensorAngle;
+ }
+
+int checkAccDec(double angle,double thisThrust){
+  int buzzerOn = 0;
+  //check uphill
+  if(angle < -angleLimit){
+    if ( thisThrust < -thrustLimit){
+      buzzerOn = 1;
+    }
+  }
+  //check downhill
+  if (angle > angleLimit) {
+    if(thisThrust > thrustLimit) {
+      buzzerOn = 1;
+    }
+  }
+  return buzzerOn;
+}
+
+ float calcThrust(float A,float B){
+   float thrust = 0;
+  
+   if (A*B > 0){            //if they are thrusting in the same direction
+      thrust = A*abs(B);    //return their product but keep one of their signs   
+   }
+    
+   return thrust;
+ }
+int checkPivot(int rpm1, int rpm2){
+  int buzzerOn = 0;
+  if(abs(rpm1) < 300 && abs(rpm2) > 500 || abs(rpm2) < 300 && abs(rpm1) > 500) {
+    buzzerOn = 1;
+  }
+  return buzzerOn;
+}
+
+int checkPendulum(float accx,float accy,float accz,int rpm1,int rpm2){
+  int buzzerOn = 0;
+  //first find out how much slope there is here
+  //float localSlope = acos(accz/9.8);
+  //next I need to know if I am rotating uphill or downhill and by how much
+  float rotation = rpm1 - rpm2;
+  Serial.println(rotation);
+  Serial.println(maxTorque);
+  Serial.println(accx);
+  Serial.println(sideAccK);
+  Serial.println(" ");
+  if (accx > sideAccK && rotation < -maxTorque || accx < -sideAccK && rotation > maxTorque){
+    buzzerOn = 1;
+  }
+  return buzzerOn;
 }
  
 void setup()
@@ -107,30 +170,8 @@ void loop() {
   rcCH5 = readChannel(4, -100, 100, 0);
   rcCH6 = readSwitch(5, false);
 
-
- 
-  // Print values to serial monitor for debugging
- // Serial.print("Ch1 = ");
- // Serial.print(rcCH1);
- 
- // Serial.print(" Ch2 = ");
- // Serial.print(rcCH2);
- 
-  //Serial.print(" Ch3 = ");
- // Serial.print(rcCH3);
- 
- // Serial.print(" Ch5 = ");
- // Serial.print(rcCH5);
- 
- // Serial.print(" Ch6 = ");
- // Serial.println(rcCH6);
-
- // Serial.print("M1 RPM = ");
-  //Serial.print(vesc1.data.rpm);
-
-  //Serial.print("M2 RPM = ");
- // Serial.print(vesc2.data.rpm);
-
+  buzzOn = 0;
+/*
   Serial.print("n =");
   Serial.println(n);
 
@@ -139,7 +180,7 @@ void loop() {
 
   Serial.print("Ax = ");
   Serial.print(mySensor.readAccelerometer(X_AXIS));
-  
+  */
   // Set speeds with channel 2 & 3 values
   MotorSpeedA = rcCH2;
   MotorSpeedB = rcCH3;
@@ -155,12 +196,25 @@ void loop() {
  mySensor.updateAccel();
  accz = mySensor.readAccelerometer(Z_AXIS);
  accx = mySensor.readAccelerometer(X_AXIS);
+ accy = mySensor.readAccelerometer(Y_AXIS);
  //if z isnt getting the full force of gravity and x is seeing some gravity
  //if z isnt seeing the full force of gravity and x is below a certain amount (sloped the other way)
  //if one wheel is moving but the other one isnt
- //conver
-  if (accz < 9.4 && accx > 1 || accz < 9.4 && accx < .3 || abs(rpm1) < 300 && abs(rpm2) > 500 || abs(rpm2) < 300 && abs(rpm1) > 500){
-    //make sure this isnt a one-off (imu noise)
+ int maxAcc = 75;
+ int maxDec = 25;
+ int thisThrust = 0;
+ angle = getAngle(accz,accy);
+ thisThrust = calcThrust(MotorSpeedA,MotorSpeedB);
+// buzzOn += checkAccDec(angle,thisThrust);   //accelerating/deccelerating on a hill
+// buzzOn += checkPivot(rpm1,rpm2);           //pivoting on one wheel
+
+ buzzOn += checkPendulum(accx,accy,accz,rpm1,rpm2);                 //attempting to rotate uphill 
+ Serial.println(buzzOn);
+
+
+//buzzmixing may give false positives
+if (buzzOn) {
+//make sure this isnt a one-off (imu noise)
     n = n+1;
   
   if (n > 4) {
